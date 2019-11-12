26d79c449142f02a764cf06b6ac128b356b5fe25
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2019-11-11 23:42:42 -0300

    Add Homebrew formula

---
+++ Formula/dx.rb
@@ -0,0 +1,25 @@
+# References:
+# - https://docs.brew.sh/Formula-Cookbook
+# - https://rubydoc.brew.sh/Formula
+class Dx < Formula
+  desc "Command-line tool to help you manage Docker resources faster"
+  homepage "https://github.com/hpedrorodrigues/dx"
+  url "https://github.com/hpedrorodrigues/dx/archive/v0.1.0.tar.gz"
+  sha256 "6ca11291f735c5527990ff8c4a944a452341b933b6736ca88d602a342880245b"
+
+  def install
+    bin.install "dx"
+    prefix.install "commands"
+
+    inreplace "#{bin}/dx" do |s|
+      s.gsub! /DX_HOME=.*/, "DX_HOME=\"#{prefix}\""
+    end
+
+    bash_completion.install "completion/dx.bash" => "dx"
+    zsh_completion.install "completion/_dx.zsh" => "_dx"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/dx --help")
+  end
+end
