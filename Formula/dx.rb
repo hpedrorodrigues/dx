17350efa91f2a454e9c7966c3eeda0aad89d3642
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2020-02-08 09:16:22 -0300

    Bump homebrew formula

--- Formula/dx.rb
+++ Formula/dx.rb
@@ -4,8 +4,8 @@
 class Dx < Formula
   desc "Command-line tool to help you manage Docker resources faster"
   homepage "https://github.com/hpedrorodrigues/dx"
-  url "https://github.com/hpedrorodrigues/dx/archive/v0.1.0.tar.gz"
-  sha256 "6ca11291f735c5527990ff8c4a944a452341b933b6736ca88d602a342880245b"
+  url "https://github.com/hpedrorodrigues/dx/archive/v0.1.1.tar.gz"
+  sha256 "576648d402dae3b81ee5f15a8b197e6f4eaa573055f3ab008a3943b182426885"
 
   def install
     bin.install "dx"
